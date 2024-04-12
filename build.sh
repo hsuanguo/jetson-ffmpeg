a2b5da4cde4347d2d0ad9c001b588408c9f6a787
hsuanguo <hsuan.guo@gmail.com>
2024-04-12 01:38:38 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -26,10 +26,69 @@ make -j$(nproc)
 sudo make install
 
 # build ffmpeg
-git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.4 --depth=1
+git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1
 cd ffmpeg
-cp ${repo_dir}/ffmpeg_patches/ffmpeg4.4_nvmpi.patch ./ffmpeg_nvmpi.patch
+cp ${repo_dir}/ffmpeg_patches/ffmpeg4.2_nvmpi.patch ./ffmpeg_nvmpi.patch
 git apply ffmpeg_nvmpi.patch
-./configure --enable-nvmpi --enable-shared --prefix=${install_dir}
+./configure \
+  --enable-nvmpi \
+  --enable-gpl \
+  --disable-stripping \
+  --enable-avresample \
+  --disable-filter=resample \
+  --enable-avisynth \
+  --enable-gnutls \
+  --enable-ladspa \
+  --enable-libaom \
+  --enable-libass \
+  --enable-libbluray \
+  --enable-libbs2b \
+  --enable-libcaca \
+  --enable-libcodec2 \
+  --enable-libflite \
+  --enable-libfontconfig \
+  --enable-libfreetype \
+  --enable-libfribidi \
+  --enable-libgme \
+  --enable-libgsm \
+  --enable-libjack \
+  --enable-libmp3lame \
+  --enable-libmysofa \
+  --enable-libopenjpeg \
+  --enable-libopenmpt \
+  --enable-libopus \
+  --enable-libpulse \
+  --enable-librsvg \
+  --enable-librubberband \
+  --enable-libshine \
+  --enable-libsnappy \
+  --enable-libsoxr \
+  --enable-libspeex \
+  --enable-libssh \
+  --enable-libtheora \
+  --enable-libtwolame \
+  --enable-libvidstab \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libwavpack \
+  --enable-libwebp \
+  --enable-libx265 \
+  --enable-libxml2 \
+  --enable-libxvid \
+  --enable-libzmq \
+  --enable-libzvbi \
+  --enable-lv2 \
+  --enable-omx \
+  --enable-openal \
+  --enable-opengl \
+  --enable-sdl2 \
+  --enable-libdc1394 \
+  --enable-libdrm \
+  --enable-libiec61883 \
+  --enable-chromaprint \
+  --enable-frei0r \
+  --enable-libx264 \
+  -enable-shared \
+  --prefix=${install_dir}
 make -j$(nproc)
 make install
