ad979adccce799bc4821dc6aea5197aa102820ad
hsuanguo <hsuan.guo@gmail.com>
2024-04-12 06:08:20 -0700

    added gpl flag

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ repo_dir=$(
 
 cd "${repo_dir}"
 
+enable_gpl=${1:-"false"}
+
 install_dir=${repo_dir}/build/install/usr/local
 
 # build mpi
@@ -30,13 +32,15 @@ git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1
 cd ffmpeg
 cp ${repo_dir}/ffmpeg_patches/ffmpeg4.2_nvmpi.patch ./ffmpeg_nvmpi.patch
 git apply ffmpeg_nvmpi.patch
+
+# if enable_gpl is true, then enable gpl
+if [ "${enable_gpl}" = "true" ]; then
+
 ./configure \
   --enable-nvmpi \
-  --enable-gpl \
   --disable-stripping \
   --enable-avresample \
   --disable-filter=resample \
-  --enable-avisynth \
   --enable-gnutls \
   --enable-ladspa \
   --enable-libaom \
@@ -59,7 +63,6 @@ git apply ffmpeg_nvmpi.patch
   --enable-libopus \
   --enable-libpulse \
   --enable-librsvg \
-  --enable-librubberband \
   --enable-libshine \
   --enable-libsnappy \
   --enable-libsoxr \
@@ -67,18 +70,14 @@ git apply ffmpeg_nvmpi.patch
   --enable-libssh \
   --enable-libtheora \
   --enable-libtwolame \
-  --enable-libvidstab \
   --enable-libvorbis \
   --enable-libvpx \
   --enable-libwavpack \
   --enable-libwebp \
-  --enable-libx265 \
   --enable-libxml2 \
-  --enable-libxvid \
   --enable-libzmq \
   --enable-libzvbi \
   --enable-lv2 \
-  --enable-omx \
   --enable-openal \
   --enable-opengl \
   --enable-sdl2 \
@@ -86,9 +85,71 @@ git apply ffmpeg_nvmpi.patch
   --enable-libdrm \
   --enable-libiec61883 \
   --enable-chromaprint \
+  --enable-shared \
+  --enable-gpl \
+  --enable-avisynth \
+  --enable-librubberband \
+  --enable-libvidstab \
+  --enable-libx265 \
+  --enable-libxvid \
+  --enable-omx \
   --enable-frei0r \
   --enable-libx264 \
-  -enable-shared \
   --prefix=${install_dir}
+else
+
+./configure \
+  --enable-nvmpi \
+  --disable-stripping \
+  --enable-avresample \
+  --disable-filter=resample \
+  --enable-gnutls \
+  --enable-ladspa \
+  --enable-libaom \
+  --enable-libass \
+  --enable-libbluray \
+  --enable-libbs2b \
+  --enable-libcaca \
+  --enable-libcodec2 \
+  --enable-libflite \
+  --enable-libfontconfig \
+  --enable-libfreetype \
+  --enable-libfribidi \
+  --enable-libgme \
+  --enable-libgsm \
+  --enable-libjack \
+  --enable-libmp3lame \
+  --enable-libmysofa \
+  --enable-libopenjpeg \
+  --enable-libopenmpt \
+  --enable-libopus \
+  --enable-libpulse \
+  --enable-librsvg \
+  --enable-libshine \
+  --enable-libsnappy \
+  --enable-libsoxr \
+  --enable-libspeex \
+  --enable-libssh \
+  --enable-libtheora \
+  --enable-libtwolame \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libwavpack \
+  --enable-libwebp \
+  --enable-libxml2 \
+  --enable-libzmq \
+  --enable-libzvbi \
+  --enable-lv2 \
+  --enable-openal \
+  --enable-opengl \
+  --enable-sdl2 \
+  --enable-libdc1394 \
+  --enable-libdrm \
+  --enable-libiec61883 \
+  --enable-chromaprint \
+  --enable-shared \
+  --prefix=${install_dir}
+fi
+
 make -j$(nproc)
 make install
